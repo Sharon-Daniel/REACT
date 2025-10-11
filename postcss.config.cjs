6831e3f8be37c825a03c470d4b1bfc255633944b
Sharon-Daniel <s.daniellawrence@gmail.com>
2025-10-11 12:15:53 +0530

    Add files via upload

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+    module.exports = {
+      plugins: {
+        // This plugin imports the Tailwind CSS library
+        tailwindcss: {},
+        // This plugin handles vendor prefixes for better browser compatibility
+        autoprefixer: {},
+      },
+    }
+    
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // ⭐️ ADD THIS block to explicitly configure PostCSS
+  css: {
+    postcss: './postcss.config.js',
+  }
+})
